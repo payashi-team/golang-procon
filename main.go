06cc84dc5b56009b9bd5941814edf9d7865ee560
hiromichi_hayashi <42857417+payashi@users.noreply.github.com>
2021-12-29 05:32:09 +0000

    palindrome phobia

--- main.go
+++ main.go
@@ -11,20 +11,37 @@ func main() {
 	var S string
 	fmt.Fscan(_r, &S)
 	ans := Solve(S)
-	fmt.Fprintf(_w, "%d\n", ans)
+	fmt.Fprintf(_w, "%s\n", ans)
 }
 
-func Solve(S string) int {
-	N:=len(S)
-	ans:=(N-1)*N
-	for i := 0; i < N; i++ {
-		if S[i]=='U'{
-			ans+=i
+func Solve(S string) string {
+	var a, b, c int
+	for i := 0; i < len(S); i++ {
+		if S[i]=='a'{
+			a++;
+		}else if S[i]=='b'{
+			b++;
 		}else{
-			ans+=N-1-i
+			c++;
 		}
 	}
-	return ans
+	max:=MaxInt([]int{a, b, c})
+	min:=-MaxInt([]int{-a, -b, -c})
+	if max-min>1{
+		return "NO"
+	}else{
+		return "YES"
+	}
+}
+
+func MaxInt(nums []int)int{
+	ret:=-int(1e5)
+	for _, v := range nums {
+		if ret<v{
+			ret = v
+		}
+	}
+	return ret
 }
 
 var _r, _w = bufio.NewReader(os.Stdin), bufio.NewWriter(os.Stdout)
