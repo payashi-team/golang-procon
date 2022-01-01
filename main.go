d993def026b2718de826ae470f74f4c4aee486c9
hiromichi_hayashi <42857417+payashi@users.noreply.github.com>
2022-01-01 02:22:09 +0000

    limited insertion

--- main.go
+++ main.go
@@ -8,22 +8,39 @@ import (
 
 func main() {
 	defer _w.Flush()
-	var A, B, C int64
-	fmt.Fscan(_r, &A, &B, &C)
-	ans := Solve(A, B, C)
-	fmt.Fprintf(_w, "%s\n", ans)
+	var N int
+	fmt.Fscan(_r, &N)
+	A := make([]int, N)
+	for i := 0; i < N; i++ {
+		fmt.Fscan(_r, &A[i])
+	}
+	ans := Solve(N, A)
+	for _, v := range ans {
+		fmt.Fprintf(_w, "%d\n", v)
+	}
 }
 
-func Solve(a, b, c int64) string {
-	if c < a+b {
-		return "No"
+func Solve(N int, A []int) []int {
+	ret:=make([]int, 0)
+	B := make([]int, N)
+	for cnt:=0;cnt<N;cnt++{
+		update:=false
+		for i := N-1; i >= 0; i--{
+			if A[i]-B[i]==1{
+				update = true
+				ret = append(ret, A[i])
+				A[i]=-1
+				for j := i+1; j < N; j++ {
+					B[j]++
+				}
+				break
+			}
+		}
+		if !update{
+			return []int{-1}
+		}
 	}
-	ans := 4*a*b < (c-(a+b))*(c-(a+b))
-	if ans {
-		return "Yes"
-	} else {
-		return "No"
-	}
+	return ret
 }
 
 var _r, _w = bufio.NewReader(os.Stdin), bufio.NewWriter(os.Stdout)
