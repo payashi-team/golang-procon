0556f2cec15b72e61a6d921a26a84d4046eee29b
hiromichi_hayashi <42857417+payashi@users.noreply.github.com>
2022-01-02 02:02:16 +0000

    range flip find route

--- main.go
+++ main.go
@@ -8,36 +8,61 @@ import (
 
 func main() {
 	defer _w.Flush()
-	var N int
-	fmt.Fscan(_r, &N)
-	A := make([]int, N)
-	for i := 0; i < N; i++ {
-		fmt.Fscan(_r, &A[i])
+	var H, W int
+	fmt.Fscan(_r, &H, &W)
+	grid := make([]byte, H*W)
+	for i := 0; i < H; i++ {
+		var S string
+		fmt.Fscan(_r, &S)
+		for j := 0; j < W; j++ {
+			grid[i*W+j] = S[j]
+		}
 	}
-	ans := Solve(N, A)
-	for _, v := range ans {
-		fmt.Fprintf(_w, "%d\n", v)
-	}
+	ans := Solve(H, W, grid)
+	fmt.Fprintf(_w, "%d\n", ans)
 }
 
-func Solve(N int, A []int) []int {
-	ret:=make([]int, 0)
-	B := make([]int, N)
-	for cnt:=0;cnt<N;cnt++{
-		update:=false
-		for i := N-1; i >= 0; i--{
-			if A[i]-B[i]==1{
-				update = true
-				ret = append(ret, A[i])
-				A[i]=-1
-				for j := i+1; j < N; j++ {
-					B[j]++
+func Solve(H, W int, grid []byte) int {
+	dp:=make([]int, H*W)
+	for i := 0; i < H; i++ {
+		for j := 0; j < W; j++ {
+			dp[i*W+j] = int(2e4)
+		}
+	}
+	if grid[0]=='#'{
+		dp[0] = 1
+	}else{
+		dp[0] = 0
+	}
+	for i := 0; i < H; i++ {
+		for j := 0; j < W; j++ {
+			cur:=i*W+j
+			if i>0{
+				top:=(i-1)*W+j
+				if grid[cur]=='#'&&grid[top]=='.'{
+					dp[cur] = MinInt(dp[cur], dp[top]+1)
+				}else{
+					dp[cur] = MinInt(dp[cur], dp[top])
 				}
-				break
+			}
+			if j>0{
+				left:=i*W+j-1
+				if grid[cur]=='#'&&grid[left]=='.'{
+					dp[cur] = MinInt(dp[cur], dp[left]+1)
+				}else{
+					dp[cur] = MinInt(dp[cur], dp[left])
+				}
 			}
 		}
-		if !update{
-			return []int{-1}
+	}
+	return dp[H*W-1]
+}
+
+func MinInt(nums ...int)int{
+	ret:=int(2e4)
+	for _, v := range nums {
+		if ret>v{
+			ret = v
 		}
 	}
 	return ret
