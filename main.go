6c6e11333c35d8d9d70a7475afe74239a9ea6209
hiromichi_hayashi <42857417+payashi@users.noreply.github.com>
2022-01-16 02:51:56 +0000

    different strokes

--- main.go
+++ main.go
@@ -4,65 +4,34 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 )
 
 func main() {
 	defer _w.Flush()
-	var H, W int
-	fmt.Fscan(_r, &H, &W)
-	grid := make([]byte, H*W)
-	for i := 0; i < H; i++ {
-		var S string
-		fmt.Fscan(_r, &S)
-		for j := 0; j < W; j++ {
-			grid[i*W+j] = S[j]
-		}
+	var N int
+	fmt.Fscan(_r, &N)
+	A := make([]int, N)
+	B := make([]int, N)
+	for i := 0; i < N; i++ {
+		fmt.Fscan(_r, &A[i], &B[i])
 	}
-	ans := Solve(H, W, grid)
+	ans := Solve(N, A, B)
 	fmt.Fprintf(_w, "%d\n", ans)
 }
 
-func Solve(H, W int, grid []byte) int {
-	dp:=make([]int, H*W)
-	for i := 0; i < H; i++ {
-		for j := 0; j < W; j++ {
-			dp[i*W+j] = int(2e4)
-		}
+func Solve(N int, A, B []int) int {
+	C := make([][]int, N)
+	for i := 0; i < N; i++ {
+		C[i] = []int{A[i], B[i]}
 	}
-	if grid[0]=='#'{
-		dp[0] = 1
-	}else{
-		dp[0] = 0
-	}
-	for i := 0; i < H; i++ {
-		for j := 0; j < W; j++ {
-			cur:=i*W+j
-			if i>0{
-				top:=(i-1)*W+j
-				if grid[cur]=='#'&&grid[top]=='.'{
-					dp[cur] = MinInt(dp[cur], dp[top]+1)
-				}else{
-					dp[cur] = MinInt(dp[cur], dp[top])
-				}
-			}
-			if j>0{
-				left:=i*W+j-1
-				if grid[cur]=='#'&&grid[left]=='.'{
-					dp[cur] = MinInt(dp[cur], dp[left]+1)
-				}else{
-					dp[cur] = MinInt(dp[cur], dp[left])
-				}
-			}
-		}
-	}
-	return dp[H*W-1]
-}
-
-func MinInt(nums ...int)int{
-	ret:=int(2e4)
-	for _, v := range nums {
-		if ret>v{
-			ret = v
+	sort.Slice(C, func(i, j int) bool { return C[i][0]+C[i][1] > C[j][0]+C[j][1] })
+	ret := 0
+	for i := 0; i < N; i++ {
+		if i%2 == 0 {
+			ret += C[i][0]
+		} else {
+			ret -= C[i][1]
 		}
 	}
 	return ret
