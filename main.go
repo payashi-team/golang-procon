ea21ebc0f8e774f9165e38a15c55c0c354f1447d
hiromichi_hayashi <42857417+payashi@users.noreply.github.com>
2022-01-26 21:57:32 +0000

    solve How to make Delicious Takoyaki

--- main.go
+++ main.go
@@ -14,45 +14,41 @@ import (
 
 func main() {
 	defer _w.Flush()
-	var N, K, M, R int
-	fmt.Fscan(_r, &N, &K, &M, &R)
-	S := make([]int, N)
-	for i := 0; i < N-1; i++ {
-		fmt.Fscan(_r, &S[i])
+	var T, N, M int
+	fmt.Fscan(_r, &T, &N)
+	A := make([]int, N)
+	for i := 0; i < N; i++ {
+		fmt.Fscan(_r, &A[i])
 	}
-	ans := Solve(N, K, M, R, S)
-	fmt.Fprintf(_w, "%d\n", ans)
+	fmt.Fscan(_r, &M)
+	B := make([]int, M)
+	for i := 0; i < M; i++ {
+		fmt.Fscan(_r, &B[i])
+	}
+	ans := Solve(T, N, M, A, B)
+	if ans {
+		fmt.Fprintf(_w, "yes\n")
+	} else {
+		fmt.Fprintf(_w, "no\n")
+	}
 }
 
-func Solve(N, K, M, R int, S []int) int {
-	sort.Slice(S, func(i, j int) bool { return S[i] > S[j] })
-	if N == K {
-		sum := 0
-		for _, v := range S {
-			sum += v
-		}
-		if sum >= K*R {
-			return 0
-		} else if sum+M < K*R {
-			return -1
-		} else {
-			return K*R - sum
-		}
+func Solve(T, N, M int, A, B []int) bool {
+	if N < M {
+		return false
 	}
-	sum:=0
-	for i := 0; i < K; i++ {
-		sum+=S[i]
-	}
-	if sum>=K*R{
-		return 0
-	}else{
-		X:=K*R-sum+S[K-1]
-		if X>M{
-			return -1
-		}else{
-			return X
+	sort.Ints(A)
+	sort.Ints(B)
+	j := 0
+	for i := 0; i < M; i++ {
+		for ; j < N && (A[j] > B[i] || B[i] > A[j]+T); j++ {
 		}
+		if j == N {
+			return false
+		}
+		j++
 	}
+	return true
 }
 
 var _r, _w = bufio.NewReader(os.Stdin), bufio.NewWriter(os.Stdout)
