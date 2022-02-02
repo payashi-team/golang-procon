2ac9682be90741916a86b4c72d084f766b0eb4cd
hiromichi_hayashi <42857417+payashi@users.noreply.github.com>
2022-02-02 00:55:19 +0000

    solve Opposite

--- main.go
+++ main.go
@@ -3,76 +3,51 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 )
 
-const (
-	INF = int(1 << 62)
-	// MOD = int(1e9 + 7)
-	MOD = 998244353
-)
+// const (
+// INF = int(1 << 62)
+// MOD = int(1e9 + 7)
+// MOD = 998244353
+// )
+
+type Cord struct {
+	x, y float64
+}
+
+func (c Cord) Norm() float64 {
+	return math.Sqrt(c.x*c.x + c.y*c.y)
+}
+
+func (c Cord) Plus(d Cord) Cord {
+	return Cord{c.x + d.x, c.y + d.y}
+}
+
+func (c Cord) Scalar(alpha float64) Cord {
+	return Cord{c.x * alpha, c.y * alpha}
+}
 
 func main() {
 	defer _w.Flush()
 	var N int
+	var P0, PM Cord
 	fmt.Fscan(_r, &N)
-	A := make([]int, N)
-	for i := 0; i < N; i++ {
-		fmt.Fscan(_r, &A[i])
-	}
-	ans := Solve(N, A)
-	fmt.Fprintf(_w, "%d\n", ans)
+	fmt.Fscan(_r, &P0.x, &P0.y)
+	fmt.Fscan(_r, &PM.x, &PM.y)
+	ans := Solve(N, P0, PM)
+	fmt.Fprintf(_w, "%.8f %.8f\n", ans.x, ans.y)
 }
 
-type UnionFind struct {
-	par, dep []int
-}
-
-func (uf *UnionFind) Root(x int) int {
-	if uf.par[x] == x {
-		return x
-	}
-	uf.par[x] = uf.par[uf.par[x]]
-	return uf.Root(uf.par[x])
-}
-
-func (uf *UnionFind) Same(a, b int) bool {
-	return uf.Root(a) == uf.Root(b)
-}
-
-func (uf *UnionFind) Unite(a, b int) {
-	a = uf.Root(a)
-	b = uf.Root(b)
-	if uf.Same(a, b) {
-		return
-	}
-	if uf.dep[a] < uf.dep[b] {
-		uf.par[a] = b
-	} else if uf.dep[a] > uf.dep[b] {
-		uf.par[b] = a
-	} else {
-		uf.par[b] = a
-		uf.dep[a]++
-	}
-}
-
-func Solve(N int, A []int) int {
-	uf := new(UnionFind)
-	uf.par = make([]int, int(2e5)+1)
-	uf.dep = make([]int, int(2e5)+1)
-	for i := 0; i <= int(2e5); i++ {
-		uf.par[i] = i
-	}
-	ret := 0
-	for i := 0; i < (N+1)/2; i++ {
-		a := A[i]
-		b := A[N-1-i]
-		if !uf.Same(a, b) {
-			uf.Unite(a, b)
-			ret++
-		}
-	}
-	return ret
+func Solve(N int, P0, PM Cord) Cord {
+	C := P0.Plus(PM).Scalar(0.5)
+	CP0 := P0.Plus(C.Scalar(-1))
+	r := CP0.Norm()
+	theta0 := math.Atan2(CP0.y, CP0.x)
+	Ry, Rx := math.Sincos(theta0 + 2*math.Pi/float64(N))
+	// fmt.Printf("C: %v\nCP0: %v\nr: %f\ntheta0: %f\n", C, CP0, r, theta0)
+	return C.Plus(Cord{Rx, Ry}.Scalar(r))
 }
 
 var _r, _w = bufio.NewReader(os.Stdin), bufio.NewWriter(os.Stdout)
