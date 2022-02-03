57d2c6f104c4c0a4b40a562b7cc0f3bba0978c12
hiromichi_hayashi <42857417+payashi@users.noreply.github.com>
2022-02-03 04:25:25 +0000

    solve NicoNico Mojiretsu

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math"
 	"os"
 )
 
@@ -13,41 +12,32 @@ import (
 // MOD = 998244353
 // )
 
-type Cord struct {
-	x, y float64
-}
-
-func (c Cord) Norm() float64 {
-	return math.Sqrt(c.x*c.x + c.y*c.y)
-}
-
-func (c Cord) Plus(d Cord) Cord {
-	return Cord{c.x + d.x, c.y + d.y}
-}
-
-func (c Cord) Scalar(alpha float64) Cord {
-	return Cord{c.x * alpha, c.y * alpha}
-}
-
 func main() {
 	defer _w.Flush()
-	var N int
-	var P0, PM Cord
-	fmt.Fscan(_r, &N)
-	fmt.Fscan(_r, &P0.x, &P0.y)
-	fmt.Fscan(_r, &PM.x, &PM.y)
-	ans := Solve(N, P0, PM)
-	fmt.Fprintf(_w, "%.8f %.8f\n", ans.x, ans.y)
+	var S string
+	fmt.Fscan(_r, &S)
+	ans := Solve(S)
+	fmt.Fprintf(_w, "%d\n", ans)
 }
 
-func Solve(N int, P0, PM Cord) Cord {
-	C := P0.Plus(PM).Scalar(0.5)
-	CP0 := P0.Plus(C.Scalar(-1))
-	r := CP0.Norm()
-	theta0 := math.Atan2(CP0.y, CP0.x)
-	Ry, Rx := math.Sincos(theta0 + 2*math.Pi/float64(N))
-	// fmt.Printf("C: %v\nCP0: %v\nr: %f\ntheta0: %f\n", C, CP0, r, theta0)
-	return C.Plus(Cord{Rx, Ry}.Scalar(r))
+func Solve(S string) int {
+	ret := 0
+	cur := 0
+	N := len(S)
+	for i := 0; i < N; i++ {
+		if i+2 > N {
+			break
+		}
+		if S[i:i+2] == "25" {
+			cur++
+			i++
+		} else {
+			ret += cur * (cur + 1) / 2
+			cur = 0
+		}
+	}
+	ret += cur * (cur + 1) / 2
+	return ret
 }
 
 var _r, _w = bufio.NewReader(os.Stdin), bufio.NewWriter(os.Stdout)
