ff545d6c527bb9435869739c8997c1a078a1ad75
hiromichi_hayashi <42857417+payashi@users.noreply.github.com>
2022-02-03 23:24:05 +0000

    solve Friends' Friends

--- main.go
+++ main.go
@@ -6,37 +6,68 @@ import (
 	"os"
 )
 
-// const (
-// INF = int(1 << 62)
+const (
+	INF = int(1 << 60)
+
 // MOD = int(1e9 + 7)
 // MOD = 998244353
-// )
+)
 
 func main() {
 	defer _w.Flush()
-	var S string
-	fmt.Fscan(_r, &S)
-	ans := Solve(S)
-	fmt.Fprintf(_w, "%d\n", ans)
+	var N, M int
+	fmt.Fscan(_r, &N, &M)
+	A := make([]int, M)
+	B := make([]int, M)
+	for i := 0; i < M; i++ {
+		fmt.Fscan(_r, &A[i], &B[i])
+	}
+	ans := Solve(N, M, A, B)
+	for _, v := range ans {
+		fmt.Fprintf(_w, "%d\n", v)
+	}
 }
 
-func Solve(S string) int {
-	ret := 0
-	cur := 0
-	N := len(S)
+func Solve(N, M int, A, B []int) []int {
+	dist := make([][]int, N)
 	for i := 0; i < N; i++ {
-		if i+2 > N {
-			break
+		dist[i] = make([]int, N)
+		for j := 0; j < N; j++ {
+			dist[i][j] = INF
 		}
-		if S[i:i+2] == "25" {
-			cur++
-			i++
-		} else {
-			ret += cur * (cur + 1) / 2
-			cur = 0
+		dist[i][i] = 0
+	}
+	for i := 0; i < M; i++ {
+		a := A[i] - 1
+		b := B[i] - 1
+		dist[a][b] = 1
+		dist[b][a] = 1
+	}
+	for k := 0; k < N; k++ {
+		for i := 0; i < N; i++ {
+			for j := 0; j < N; j++ {
+				dist[i][j] = MinInt(dist[i][j], dist[i][k]+dist[k][j])
+			}
 		}
 	}
-	ret += cur * (cur + 1) / 2
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		for j := 0; j < N; j++ {
+			if dist[i][j] == 2 {
+				ret[i]++
+			}
+		}
+	}
+	return ret
+}
+
+func MinInt(nums ...int) int {
+	ret := INF
+	for _, v := range nums {
+		if ret > v {
+			ret = v
+		}
+	}
 	return ret
 }
 
