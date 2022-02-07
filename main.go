fecab3db0ac6eeb6979c9beb66cf172a254eb0b3
hiromichi_hayashi <42857417+payashi@users.noreply.github.com>
2022-02-07 02:36:28 +0000

    solve Sequence Sum

--- main.go
+++ main.go
@@ -14,60 +14,30 @@ const (
 
 func main() {
 	defer _w.Flush()
-	var N, Q int
-	fmt.Fscan(_r, &N, &Q)
-	L := make([]int, Q)
-	R := make([]int, Q)
-	for i := 0; i < Q; i++ {
-		fmt.Fscan(_r, &L[i], &R[i])
-	}
-	ans := Solve(N, Q, L, R)
-	if ans {
-		fmt.Fprintf(_w, "Yes\n")
-	} else {
-		fmt.Fprintf(_w, "No\n")
-	}
+	var N, X, M int
+	fmt.Fscan(_r, &N, &X, &M)
+	ans := Solve(N, X, M)
+	fmt.Fprintf(_w, "%d\n", ans)
 }
 
-type UnionFind struct {
-	par, dep []int
-}
-
-func (uf *UnionFind) Root(x int) int {
-	if uf.par[x] == x {
-		return x
-	}
-	uf.par[x] = uf.par[uf.par[x]]
-	return uf.Root(uf.par[x])
-}
-
-func (uf *UnionFind) Unite(a, b int) {
-	a = uf.Root(a)
-	b = uf.Root(b)
-	if a == b {
-		return
-	}
-	if uf.dep[a] < uf.dep[b] {
-		uf.par[a] = b
-	} else {
-		uf.par[b] = a
-		if uf.dep[a] == uf.dep[b] {
-			uf.dep[a]++
+func Solve(N, X, M int) int {
+	a := make([]int, M+1)
+	cnt := make(map[int]int)
+	a[1] = X
+	sum := make([]int, M+1)
+	for i := 1; i < M; i++ {
+		sum[i] = sum[i-1] + a[i]
+		cnt[a[i]] = i
+		a[i+1] = a[i] * a[i] % M
+		if cnt[a[i+1]] > 0 {
+			s := i + 1
+			t := cnt[a[i+1]]
+			sum[s] = sum[s-1] + a[s]
+			T := s - t
+			return (sum[s]-sum[t])*((N-t+1)/T) + sum[(N-t+1)%T+(t-1)]
 		}
 	}
-}
-
-func Solve(N, Q int, L, R []int) bool {
-	uf := new(UnionFind)
-	uf.par = make([]int, N+1)
-	uf.dep = make([]int, N+1)
-	for i := 0; i <= N; i++ {
-		uf.par[i] = i
-	}
-	for i := 0; i < Q; i++ {
-		uf.Unite(L[i]-1, R[i])
-	}
-	return uf.Root(0) == uf.Root(N)
+	return 0
 }
 
 var _r, _w = bufio.NewReader(os.Stdin), bufio.NewWriter(os.Stdout)
