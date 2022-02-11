22b326de64f189a5fba6656d87990a4c80369dee
hiromichi_hayashi <42857417+payashi@users.noreply.github.com>
2022-02-11 02:00:18 +0000

    solve Transformable Teacher

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 )
 
 const (
@@ -14,57 +15,48 @@ const (
 
 func main() {
 	defer _w.Flush()
-	var N int
-	fmt.Fscan(_r, &N)
-	A := make([][]int, 2*N)
-	for i := 0; i < 2*N; i++ {
-		A[i] = make([]int, 2*N)
+	var N, M int
+	fmt.Fscan(_r, &N, &M)
+	H := make([]int, N)
+	W := make([]int, M)
+	for i := 0; i < N; i++ {
+		fmt.Fscan(_r, &H[i])
 	}
-	for i := 0; i < 2*N-1; i++ {
-		for j := i + 1; j < 2*N; j++ {
-			fmt.Fscan(_r, &A[i][j])
-			A[j][i] = A[i][j]
-		}
+	for i := 0; i < M; i++ {
+		fmt.Fscan(_r, &W[i])
 	}
-	ans := Solve(N, A)
+	ans := Solve(N, M, H, W)
 	fmt.Fprintf(_w, "%d\n", ans)
 }
 
-func Solve(N int, A [][]int) int {
-	var dfs func(int, []bool) int
-	dfs = func(score int, used []bool) int {
-		pos := -1
-		for i, v := range used {
-			if !v {
-				pos = i
-				break
-			}
-		}
-		if pos < 0 {
-			// fmt.Printf("used: %v, score: %d\n", used, score)
-			return score
-		}
-		used[pos] = true
-		ret := 0
-		for i := 0; i < 2*N; i++ {
-			if used[i] {
-				continue
-			}
-			used[i] = true
-			ret = MaxInt(ret, dfs(score^A[pos][i], used))
-			used[i] = false
-		}
-		used[pos] = false
-		return ret
+func Solve(N, M int, H, W []int) int {
+	sort.Ints(H)
+	SumEvens := make([]int, N/2+1) // (0, 1) +... (2*k+2, 2*k+3)
+	for i := 1; 2*i-1 < N; i++ {
+		SumEvens[i] = SumEvens[i-1] + H[2*i-1] - H[2*i-2]
 	}
-	used := make([]bool, 2*N)
-	return dfs(0, used)
+	SumOdds := make([]int, N/2+1) // (1, 2) + ... (2*k-1, 2*k)
+	for i := 1; 2*i < N; i++ {
+		SumOdds[i] = SumOdds[i-1] + H[2*i] - H[2*i-1]
+	}
+	ret := INF
+	for _, w := range W {
+		idx := sort.SearchInts(H, w)
+		diff := SumEvens[idx/2] + SumOdds[N/2] - SumOdds[idx/2]
+		if idx&1 == 0 {
+			diff += H[idx] - w
+		} else {
+			diff += w - H[idx-1]
+		}
+		ret = MinInt(ret, diff)
+	}
+	return ret
 }
 
-func MaxInt(nums ...int) int {
-	ret := 0
+func MinInt(nums ...int) int {
+	ret := INF
 	for _, v := range nums {
-		if ret < v {
+		if ret > v {
 			ret = v
 		}
 	}
