60a90a661e140f3169026806d8d554a4c0c7e5c0
hiromichi_hayashi <42857417+payashi@users.noreply.github.com>
2022-02-17 11:53:32 +0000

    solve Not Overflow

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"sort"
 )
 
 const (
@@ -15,50 +14,19 @@ const (
 
 func main() {
 	defer _w.Flush()
-	var T int
-	fmt.Fscan(_r, &T)
-	R := make([]int, T)
-	G := make([]int, T)
-	B := make([]int, T)
-	for i := 0; i < T; i++ {
-		fmt.Fscan(_r, &R[i], &G[i], &B[i])
-	}
-	ans := Solve(T, R, G, B)
-	for _, v := range ans {
-		fmt.Fprintf(_w, "%d\n", v)
+	var N int
+	fmt.Fscan(_r, &N)
+	ans := Solve(N)
+	if ans {
+		fmt.Fprintf(_w, "Yes\n")
+	} else {
+		fmt.Fprintf(_w, "No\n")
 	}
 }
 
-func Solve(T int, R, G, B []int) []int {
-	ret := make([]int, T)
-	for i := 0; i < T; i++ {
-		ret[i] = INF
-		arr := []int{R[i], G[i], B[i]}
-		sort.Ints(arr)
-		if (arr[2]-arr[1])%3 == 0 {
-			ret[i] = MinInt(ret[i], arr[2])
-		}
-		if (arr[2]-arr[0])%3 == 0 {
-			ret[i] = MinInt(ret[i], arr[2])
-		}
-		if (arr[1]-arr[0])%3 == 0 {
-			ret[i] = MinInt(ret[i], arr[1])
-		}
-		if ret[i] == INF {
-			ret[i] = -1
-		}
-	}
-	return ret
-}
-
-func MinInt(nums ...int) int {
-	ret := INF
-	for _, v := range nums {
-		if ret > v {
-			ret = v
-		}
-	}
-	return ret
+func Solve(N int) bool {
+	const M = int(1 << 31)
+	return -M <= N && N < M
 }
 
 var _r, _w = bufio.NewReader(os.Stdin), bufio.NewWriter(os.Stdout)
