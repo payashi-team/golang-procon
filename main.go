0d0c69d5c366852764a0a26891e9738cc5d94b1b
hiromichi_hayashi <42857417+payashi@users.noreply.github.com>
2022-02-18 12:22:00 +0000

    solve Folia

--- main.go
+++ main.go
@@ -16,17 +16,47 @@ func main() {
 	defer _w.Flush()
 	var N int
 	fmt.Fscan(_r, &N)
-	ans := Solve(N)
-	if ans {
-		fmt.Fprintf(_w, "Yes\n")
-	} else {
-		fmt.Fprintf(_w, "No\n")
+	A := make([]int, N+1)
+	for i := 0; i <= N; i++ {
+		fmt.Fscan(_r, &A[i])
 	}
+	ans := Solve(N, A)
+	fmt.Fprintf(_w, "%d\n", ans)
 }
 
-func Solve(N int) bool {
-	const M = int(1 << 31)
-	return -M <= N && N < M
+type Range struct {
+	min, max int
+}
+
+func Solve(N int, A []int) int {
+	ret := make([]Range, N+1)
+	ret[N] = Range{A[N], A[N]}
+	for i := N - 1; i >= 0; i-- {
+		ret[i].min = (ret[i+1].min+1)/2 + A[i]
+		ret[i].max = ret[i+1].max + A[i]
+	}
+	ret[0].max = 1
+	if ret[0].max < 1 || 1 < ret[0].min {
+		return -1
+	}
+	for i := 1; i <= N; i++ {
+		ret[i].max = MinInt((ret[i-1].max-A[i-1])*2, ret[i].max)
+	}
+	sum := 0
+	for i := 0; i <= N; i++ {
+		sum += ret[i].max
+	}
+	return sum
+}
+
+func MinInt(nums ...int) int {
+	ret := INF
+	for _, v := range nums {
+		if ret > v {
+			ret = v
+		}
+	}
+	return ret
 }
 
 var _r, _w = bufio.NewReader(os.Stdin), bufio.NewWriter(os.Stdout)
