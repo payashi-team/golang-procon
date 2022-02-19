326fc074f44b1a5fb2d425aad2d136d3f5cf8c1d
hiromichi_hayashi <42857417+payashi@users.noreply.github.com>
2022-02-19 03:16:34 +0000

    solve Columns

--- main.go
+++ main.go
@@ -3,54 +3,59 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 )
 
 const (
-	INF = int(1 << 60)
+	INF = int(1 << 61)
 	// MOD = int(1e9 + 7)
 	MOD = 998244353
 )
 
 func main() {
 	defer _w.Flush()
-	var N int
-	fmt.Fscan(_r, &N)
-	A := make([]int, N+1)
-	for i := 0; i <= N; i++ {
-		fmt.Fscan(_r, &A[i])
+	var N, K int
+	fmt.Fscan(_r, &N, &K)
+	S := make([]int, N)
+	for i := 0; i < N; i++ {
+		fmt.Fscan(_r, &S[i])
 	}
-	ans := Solve(N, A)
+	ans := Solve(N, K, S)
 	fmt.Fprintf(_w, "%d\n", ans)
 }
 
-type Range struct {
-	min, max int
-}
-
-func Solve(N int, A []int) int {
-	ret := make([]Range, N+1)
-	ret[N] = Range{A[N], A[N]}
-	for i := N - 1; i >= 0; i-- {
-		ret[i].min = (ret[i+1].min+1)/2 + A[i]
-		ret[i].max = ret[i+1].max + A[i]
+func Solve(N, K int, S []int) int {
+	min := INF
+	for _, v := range S {
+		min = MinInt(min, v)
 	}
-	ret[0].max = 1
-	if ret[0].max < 1 || 1 < ret[0].min {
-		return -1
+	if min == 0 {
+		return N
+	} else if K == 0 {
+		return 0
 	}
-	for i := 1; i <= N; i++ {
-		ret[i].max = MinInt((ret[i-1].max-A[i-1])*2, ret[i].max)
+	r := 0
+	cur := 1
+	ret := 0
+	for l := 0; l < N; l++ {
+		for r < N && cur*S[r] <= K {
+			cur *= S[r]
+			r++
+		}
+		// fmt.Printf("[%d, %d): %d\n", l, r, cur)
+		ret = -MinInt(-ret, -(r - l))
+		if l == r {
+			r++
+		} else {
+			cur /= S[l]
+		}
 	}
-	sum := 0
-	for i := 0; i <= N; i++ {
-		sum += ret[i].max
-	}
-	return sum
+	return ret
 }
 
 func MinInt(nums ...int) int {
-	ret := INF
+	ret := math.MaxInt64
 	for _, v := range nums {
 		if ret > v {
 			ret = v
