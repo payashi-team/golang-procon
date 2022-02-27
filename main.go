39c80e1dd26736eee24046518958d54c334cec6f
hiromichi_hayashi <42857417+payashi@users.noreply.github.com>
2022-02-27 07:46:39 +0000

    solve Putting Candies

--- main.go
+++ main.go
@@ -15,24 +15,37 @@ const (
 
 func main() {
 	defer _w.Flush()
-	var N, A, B int
-	fmt.Fscan(_r, &N, &A, &B)
-	ans := Solve(N, A, B)
-	if ans {
-		fmt.Fprintf(_w, "Takahashi\n")
-	} else {
-		fmt.Fprintf(_w, "Aoki\n")
+	var N, K int
+	fmt.Fscan(_r, &N, &K)
+	A := make([]int, N)
+	for i := 0; i < N; i++ {
+		fmt.Fscan(_r, &A[i])
 	}
+	ans := Solve(N, K, A)
+	fmt.Fprintf(_w, "%d\n", ans)
 }
 
-func Solve(N, A, B int) bool {
-	if A == B {
-		return N%(A+1) != 0
-	} else if A > B {
-		return true
-	} else {
-		return N <= A
+func Solve(N, K int, A []int) int {
+	sum := make([]int, N+1)
+	pos := 0
+	used := make([]int, N)
+	for i := 0; i < N; i++ {
+		used[i] = -1
 	}
+	for t := 0; t < K; t++ {
+		if used[pos] >= 0 {
+			// the end of the loop
+			p := used[pos]
+			l := t - p
+			lcost := sum[t] - sum[p]
+			a, b := (K-p)/l, (K-p)%l
+			return sum[p] + lcost*a + (sum[p+b] - sum[p])
+		}
+		used[pos] = t
+		sum[t+1] = sum[t] + A[pos]
+		pos = sum[t+1] % N
+	}
+	return sum[K]
 }
 
 func AbsInt(x int) int {
