822056c36a90aa71f2bdffdd691ab6b31d3c6ea8
hiromichi_hayashi <42857417+payashi@users.noreply.github.com>
2022-03-06 00:16:10 +0000

    solve Sorting Queries

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"container/heap"
 	"fmt"
 	"math"
 	"os"
@@ -13,20 +14,86 @@ const (
 	// MOD = 998244353
 )
 
+type Query struct {
+	c, x int
+}
+
 func main() {
 	defer _w.Flush()
-	var a, b int
-	fmt.Fscan(_r, &a, &b)
-	ans := Solve(a, b)
-	if ans {
-		fmt.Fprintf(_w, "Yes\n")
-	} else {
-		fmt.Fprintf(_w, "No\n")
+	var Q int
+	fmt.Fscan(_r, &Q)
+	queries := make([]Query, Q)
+	for i := 0; i < Q; i++ {
+		var c, x int
+		fmt.Fscan(_r, &c)
+		if c == 1 {
+			fmt.Fscan(_r, &x)
+		}
+		queries[i] = Query{c, x}
+
+	}
+	Solve(Q, queries)
+}
+
+func Solve(Q int, queries []Query) {
+	que := make([]int, 0)
+	pq := make(PQueue, 0)
+	heap.Init(&pq)
+	for _, q := range queries {
+		switch q.c {
+		case 1:
+			que = append(que, q.x)
+		case 2:
+			if pq.Len() > 0 {
+				item := heap.Pop(&pq).(*Item)
+				fmt.Fprintf(_w, "%d\n", item.val)
+			} else {
+				fmt.Fprintf(_w, "%d\n", que[0])
+				que = que[1:]
+			}
+		case 3:
+			for _, v := range que {
+				heap.Push(&pq, &Item{-v, v, -1})
+			}
+			que = []int{}
+		}
 	}
 }
 
-func Solve(a, b int) bool {
-	return AbsInt(a-b)%8 == 1
+type Item struct {
+	priority, val, index int
+}
+
+type PQueue []*Item
+
+func (pq PQueue) Len() int {
+	return len(pq)
+}
+
+func (pq PQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq PQueue) Less(i, j int) bool {
+	return pq[i].priority > pq[j].priority
+}
+
+func (pq *PQueue) Push(x interface{}) {
+	item := x.(*Item)
+	item.index = len(*pq)
+	*pq = append(*pq, item)
+}
+
+func (pq *PQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	item.index = -1
+	old[n-1] = nil
+	*pq = old[:n-1]
+	return item
 }
 
 func AbsInt(x int) int {
