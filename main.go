1c9247656ac1cf77ecd4d314c3cfd9de4edfb4e3
hiromichi_hayashi <42857417+payashi@users.noreply.github.com>
2022-03-09 01:21:08 +0000

    solve Bomber

--- main.go
+++ main.go
@@ -13,17 +13,63 @@ const (
 	// MOD = 998244353
 )
 
-func main() {
-	defer _w.Flush()
-	var H int
-	fmt.Fscan(_r, &H)
-	ans := Solve(H)
-	fmt.Fprintf(_w, "%.8f\n", ans)
+type Point struct {
+	h, w int
 }
 
-func Solve(H int) float64 {
-	h := float64(H)
-	return math.Sqrt(h * (float64(128e5) + h))
+func main() {
+	defer _w.Flush()
+	var H, W, M int
+	fmt.Fscan(_r, &H, &W, &M)
+	targets := make([]Point, M)
+	for i := 0; i < M; i++ {
+		fmt.Fscan(_r, &targets[i].h, &targets[i].w)
+	}
+	ans := Solve(H, W, M, targets)
+	fmt.Fprintf(_w, "%d\n", ans)
+}
+
+func Solve(H, W, M int, targets []Point) int {
+	cnth := make([]int, H)
+	cntw := make([]int, W)
+	filled := make(map[Point]bool)
+	for _, t := range targets {
+		cnth[t.h-1]++
+		cntw[t.w-1]++
+		filled[t] = true
+	}
+	maxh := -1
+	hs := make([]int, 0)
+	for i := 0; i < H; i++ {
+		if cnth[i] >= maxh {
+			if cnth[i] > maxh {
+				hs = []int{}
+			}
+			maxh = cnth[i]
+			hs = append(hs, i)
+		}
+	}
+	maxw := -1
+	ws := make([]int, 0)
+	for i := 0; i < W; i++ {
+		if cntw[i] >= maxw {
+			if cntw[i] > maxw {
+				ws = []int{}
+			}
+			maxw = cntw[i]
+			ws = append(ws, i)
+		}
+	}
+	ret := maxh + maxw - 1
+	for _, h := range hs {
+		for _, w := range ws {
+			if !filled[Point{h + 1, w + 1}] {
+				ret++
+				return ret
+			}
+		}
+	}
+	return ret
 }
 
 func AbsInt(x int) int {
