34b4d72da76e6ddcf6ce25b5b6a4235c546b321d
hiromichi_hayashi <42857417+payashi@users.noreply.github.com>
2022-03-12 01:25:35 +0000

    solve Queen on Grid

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"math"
 	"os"
-	"sort"
 )
 
 const (
@@ -14,72 +13,52 @@ const (
 	// MOD = 998244353
 )
 
-type Query struct {
-	command string
-	n, m    int
-}
-
 func main() {
 	defer _w.Flush()
-	var Q, L int
-	fmt.Fscan(_r, &Q, &L)
-	Solve(Q, L)
+	var H, W int
+	fmt.Fscan(_r, &H, &W)
+	field := make([]string, H)
+	for i := 0; i < H; i++ {
+		fmt.Fscan(_r, &field[i])
+	}
+	ans := Solve(H, W, field)
+	fmt.Fprintf(_w, "%d\n", ans)
 }
 
-type Item struct {
-	val, acc int
-}
-
-func Solve(Q, L int) {
-	size := 0
-	stack := make([]Item, 0)
-	safe := true
-	for i := 0; i < Q; i++ {
-		var s string
-		var n, m int
-		fmt.Fscan(_r, &s)
-		if s == "Push" {
-			fmt.Fscan(_r, &n, &m)
-		} else if s == "Pop" {
-			fmt.Fscan(_r, &n)
-		}
-		q := Query{s, n, m}
-		if !safe {
-			continue
-		}
-		if q.command == "Push" {
-			size += q.n
-			if size > L {
-				fmt.Fprintf(_w, "FULL\n")
-				safe = false
-				continue
-			}
-			stack = append(stack, Item{q.m, size})
-		} else if q.command == "Pop" {
-			size -= q.n
-			if size < 0 {
-				fmt.Fprintf(_w, "EMPTY\n")
-				safe = false
-				continue
-			}
-			idx := sort.Search(len(stack), func(j int) bool { return stack[j].acc >= size })
-			stack = stack[:idx+1]
-			stack[idx].acc = size
-		} else if q.command == "Top" {
-			if size == 0 {
-				fmt.Fprintf(_w, "EMPTY\n")
-				safe = false
-				continue
-			}
-			fmt.Fprintf(_w, "%d\n", stack[len(stack)-1].val)
-		} else if q.command == "Size" {
-			fmt.Fprintf(_w, "%d\n", size)
-		}
-		// fmt.Fprintf(_w, "%v\n", stack)
+func Solve(H, W int, field []string) int {
+	dp := make([][]int, H)
+	r := make([][]int, H)
+	d := make([][]int, H)
+	rd := make([][]int, H)
+	for i := 0; i < H; i++ {
+		dp[i] = make([]int, W)
+		r[i] = make([]int, W)
+		d[i] = make([]int, W)
+		rd[i] = make([]int, W)
 	}
-	if safe {
-		fmt.Fprintf(_w, "SAFE\n")
+	dp[H-1][W-1] = 1
+	for h := H - 1; h >= 0; h-- {
+		for w := W - 1; w >= 0; w-- {
+			if (h == H-1 && w == W-1) || field[h][w] == '#' {
+				continue
+			}
+			if w+1 < W && field[h][w+1] != '#' {
+				r[h][w] = r[h][w+1] + dp[h][w+1]
+				r[h][w] %= MOD
+			}
+			if h+1 < H && field[h+1][w] != '#' {
+				d[h][w] = d[h+1][w] + dp[h+1][w]
+				d[h][w] %= MOD
+			}
+			if w+1 < W && h+1 < H && field[h+1][w+1] != '#' {
+				rd[h][w] = rd[h+1][w+1] + dp[h+1][w+1]
+				rd[h][w] %= MOD
+			}
+			dp[h][w] = r[h][w] + d[h][w] + rd[h][w]
+			dp[h][w] %= MOD
+		}
 	}
+	return dp[0][0]
 }
 
 func AbsInt(x int) int {
