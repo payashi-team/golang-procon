379532385a7940a14ec4bcc36eaacd03d98eaf35
hiromichi_hayashi <42857417+payashi@users.noreply.github.com>
2022-03-19 10:41:48 +0000

    solve Almost Everywhere Zero

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"math"
-	"math/bits"
 	"os"
+	"strconv"
 )
 
 const (
@@ -16,50 +16,45 @@ const (
 
 func main() {
 	defer _w.Flush()
-	var N, M int
-	fmt.Fscan(_r, &N, &M)
-	A := make([]int, N)
-	for i := 0; i < N; i++ {
-		fmt.Fscan(_r, &A[i])
-	}
-	B := make([]int, M)
-	C := make([]int, M)
-	for i := 0; i < M; i++ {
-		var c int
-		fmt.Fscan(_r, &B[i], &c)
-		for j := 0; j < c; j++ {
-			var I int
-			fmt.Fscan(_r, &I)
-			I--
-			C[i] |= 1 << I
-		}
-	}
-	ans := Solve(N, M, A, B, C)
+	var N string
+	var K int
+	fmt.Fscan(_r, &N, &K)
+	ans := Solve(N, K)
 	fmt.Fprintf(_w, "%d\n", ans)
 }
 
-func Solve(N, M int, A, B, C []int) int {
-	bit := 1<<9 - 1
-	max := -1
-	for bit < 1<<N {
-		// fmt.Printf("%b\n", bit)
-		score := 0
-		for i := 0; i < N; i++ {
-			if (bit>>i)&1 == 1 {
-				score += A[i]
-			}
-		}
-		for i := 0; i < M; i++ {
-			if bits.OnesCount(uint(bit&C[i])) >= 3 {
-				score += B[i]
-			}
-		}
-		max = MaxInt(max, score)
-		x := bit & -bit
-		y := bit + x
-		bit = ((bit &^ y) / x >> 1) | y
+func Solve(N string, K int) int {
+	L := len(N)
+	dp := make([][][2]int, L+1)
+	for i := 0; i <= L; i++ {
+		dp[i] = make([][2]int, K+1)
 	}
-	return max
+	dp[0][0][0] = 1
+	for i := 0; i < L; i++ {
+		v, _ := strconv.Atoi(N[i : i+1])
+		for j := 0; j <= K; j++ {
+			for k := 0; k < 2; k++ {
+				for d := 0; d < 10; d++ {
+					var ni, nj, nk = i + 1, j, k
+					if d != 0 {
+						nj++
+					}
+					if nj > K {
+						continue
+					}
+					if k == 0 {
+						if d > v {
+							continue
+						} else if d < v {
+							nk = 1
+						}
+					}
+					dp[ni][nj][nk] += dp[i][j][k]
+				}
+			}
+		}
+	}
+	return dp[L][K][0] + dp[L][K][1]
 }
 
 func AbsInt(x int) int {
