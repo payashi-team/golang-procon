59f1af3fb487f1f45d0c44bd7d31ad571534969d
hiromichi_hayashi <42857417+payashi@users.noreply.github.com>
2022-03-29 08:50:35 +0000

    solve Sushi

--- main.go
+++ main.go
@@ -17,34 +17,52 @@ func main() {
 	defer _w.Flush()
 	var N int
 	fmt.Fscan(_r, &N)
-	ps := make([]float64, N)
+	A := make([]int, N)
 	for i := 0; i < N; i++ {
-		fmt.Fscan(_r, &ps[i])
+		fmt.Fscan(_r, &A[i])
 	}
-	ans := Solve(N, ps)
+	ans := Solve(N, A)
 	fmt.Fprintf(_w, "%.12f\n", ans)
 }
 
-func Solve(N int, ps []float64) float64 {
-	dp := make([][]float64, N+1)
-	for i := 0; i <= N; i++ {
-		dp[i] = make([]float64, N+1)
+func Solve(N int, A []int) float64 {
+	type State struct {
+		a, b, c int
 	}
-	dp[0][0] = 1.0
-	for i := 0; i < N; i++ {
-		for j := 0; j < N; j++ {
-			if dp[i][j] == 0 {
-				continue
-			}
-			dp[i+1][j] += dp[i][j] * (1 - ps[i])
-			dp[i+1][j+1] += dp[i][j] * ps[i]
+	var init State
+	for _, v := range A {
+		if v == 1 {
+			init.a++
+		} else if v == 2 {
+			init.b++
+		} else if v == 3 {
+			init.c++
 		}
 	}
-	ans := 0.0
-	for i := N/2 + 1; i <= N; i++ {
-		ans += dp[N][i]
+	n := float64(N)
+	var dp [301][301][301]float64
+	for c := 0; c <= N; c++ {
+		for b := 0; b <= N; b++ {
+			for a := 0; a <= N; a++ {
+				if a+b+c == 0 {
+					continue
+				}
+				val := 1.0
+				if a > 0 {
+					val += dp[a-1][b][c] * float64(a) / n
+				}
+				if b > 0 {
+					val += dp[a+1][b-1][c] * float64(b) / n
+				}
+				if c > 0 {
+					val += dp[a][b+1][c-1] * float64(c) / n
+				}
+				val *= n / float64(a+b+c)
+				dp[a][b][c] = val
+			}
+		}
 	}
-	return ans
+	return dp[init.a][init.b][init.c]
 }
 
 func AbsInt(x int) int {
