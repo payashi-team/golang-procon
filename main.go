194567e19ab7054c727e6110440cab5a7f30117f
hiromichi_hayashi <42857417+payashi@users.noreply.github.com>
2022-04-07 16:26:27 +0000

    solve Get Closer

--- main.go
+++ main.go
@@ -13,57 +13,17 @@ const (
 	// MOD = 998244353
 )
 
-type Edge struct {
-	x, y int
-}
-
 func main() {
 	defer _w.Flush()
-	var N int
-	fmt.Fscan(_r, &N)
-	E := make([][]int, N)
-	for i := 0; i < N-1; i++ {
-		var x, y int
-		fmt.Fscan(_r, &x, &y)
-		x--
-		y--
-		E[x] = append(E[x], y)
-		E[y] = append(E[y], x)
-	}
-	ans := Solve(N, E)
-	fmt.Fprintf(_w, "%d\n", ans)
+	var a, b float64
+	fmt.Fscan(_r, &a, &b)
+	ans := Solve(a, b)
+	fmt.Fprintf(_w, "%.10f %.10f\n", ans[0], ans[1])
 }
 
-func Solve(N int, E [][]int) int {
-	used := make([]bool, N)
-	memo := make(map[[2]int]int)
-	var dfs func(int, int) int // c: 0=white, 1=black
-	dfs = func(v, c int) int {
-		if memo[[2]int{v, c}] > 0 {
-			return memo[[2]int{v, c}]
-		}
-		// なんでおなじ引数で複数回よばれてる？？木構造なのに
-		// 色のバリエーションがあるからや！せやメモ化しよ
-		// paint in color c
-		ret := 1
-		for _, u := range E[v] {
-			if used[u] {
-				continue
-			}
-			used[u] = true
-			if c == 0 {
-				ret *= dfs(u, 0) + dfs(u, 1)
-			} else {
-				ret *= dfs(u, 0)
-			}
-			ret %= MOD
-			used[u] = false
-		}
-		memo[[2]int{v, c}] = ret
-		return ret
-	}
-	used[0] = true
-	return (dfs(0, 0) + dfs(0, 1)) % MOD
+func Solve(a, b float64) [2]float64 {
+	L := math.Sqrt(a*a + b*b)
+	return [2]float64{a / L, b / L}
 }
 
 func AbsInt(x int) int {
