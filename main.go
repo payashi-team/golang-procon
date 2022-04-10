32666d95f279a4311892187e7c5ff89f0d60dd35
hiromichi_hayashi <42857417+payashi@users.noreply.github.com>
2022-04-10 08:20:54 +0000

    solve Permutation

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"math"
 	"os"
-	"strconv"
 )
 
 const (
@@ -16,44 +15,37 @@ const (
 
 func main() {
 	defer _w.Flush()
-	var K string
-	var D int
-	fmt.Fscan(_r, &K, &D)
-	ans := Solve(K, D)
+	var N int
+	var S string
+	fmt.Fscan(_r, &N, &S)
+	ans := Solve(N, S)
 	fmt.Fprintf(_w, "%d\n", ans)
 }
 
-func Solve(K string, D int) int {
-	N := len(K)
-	A := make([]int, N)
-	for i := 0; i < N; i++ {
-		v, _ := strconv.Atoi(string(K[N-1-i]))
-		A[i] = v
-	}
-	dp := make([][][]int, N+1) // dp(i, j, k) := i digits, %=j(mod D), <=K(k=1)
+func Solve(N int, S string) int {
+	dp := make([][]int, N+1) // dp(i, j) := decide i, #greater than last = j
 	for i := 0; i <= N; i++ {
-		dp[i] = make([][]int, D)
-		for j := 0; j < D; j++ {
-			dp[i][j] = make([]int, 2)
+		dp[i] = make([]int, N+1)
+	}
+	for j := 0; j <= N-1; j++ {
+		dp[1][j] = 1
+	}
+	for i := 1; i < N; i++ {
+		sum := make([]int, N+2) // sum[j+1] := dp[i][0]+...+dp[i][j]
+		for j := 1; j <= N+1; j++ {
+			sum[j] = sum[j-1] + dp[i][j-1]
+		}
+		for j := 0; j <= N; j++ {
+			if S[i-1] == '<' {
+				dp[i+1][j] += sum[N-i+1] - sum[j+1]
+				dp[i+1][j] %= MOD
+			} else {
+				dp[i+1][j] += sum[j+1]
+				dp[i+1][j] %= MOD
+			}
 		}
 	}
-	dp[0][0][0] = 1
-	dp[0][0][1] = 1
-	for i := 0; i < N; i++ {
-		for j := 0; j < D; j++ {
-			for t := 0; t < A[i]; t++ {
-				dp[i+1][(j+t)%D][1] += dp[i][j][0]
-				dp[i+1][(j+t)%D][1] %= MOD
-			}
-			dp[i+1][(j+A[i])%D][1] += dp[i][j][1]
-			dp[i+1][(j+A[i])%D][1] %= MOD
-			for t := 0; t < 10; t++ {
-				dp[i+1][(j+t)%D][0] += dp[i][j][0]
-				dp[i+1][(j+t)%D][0] %= MOD
-			}
-		}
-	}
-	return (dp[N][0][1] - 1 + MOD) % MOD
+	return dp[N][0]
 }
 
 func AbsInt(x int) int {
