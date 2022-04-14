e942b5f847b19dcf6f6f436ada7270efd25d1d64
hiromichi_hayashi <42857417+payashi@users.noreply.github.com>
2022-04-14 13:01:46 +0000

    solve Happy Birthday!2

--- main.go
+++ main.go
@@ -15,15 +15,63 @@ const (
 
 func main() {
 	defer _w.Flush()
-	var a, b, c, d int
-	fmt.Fscan(_r, &a, &b, &c, &d)
-	takahashi := 60*60*a + 60*b
-	aoki := 60*60*c + 60*d + 1
-	if takahashi > aoki {
-		fmt.Fprintf(_w, "Aoki\n")
-	} else {
-		fmt.Fprintf(_w, "Takahashi\n")
+	var N int
+	fmt.Fscan(_r, &N)
+	A := make([]int, N)
+	for i := 0; i < N; i++ {
+		fmt.Fscan(_r, &A[i])
 	}
+	Solve(N, A)
+}
+
+// log2(201)<8
+func Solve(N int, A []int) {
+	PrintBC := func(b, c int) {
+		fmt.Fprintf(_w, "Yes\n")
+		B := make([]int, 0)
+		bcnt := 0
+		for i := 0; i < N; i++ {
+			if b>>i&1 == 1 {
+				B = append(B, i)
+				bcnt++
+			}
+		}
+		C := make([]int, 0)
+		ccnt := 0
+		for i := 0; i < N; i++ {
+			if c>>i&1 == 1 {
+				C = append(C, i)
+				ccnt++
+			}
+		}
+		fmt.Fprintf(_w, "%d ", bcnt)
+		for _, v := range B {
+			fmt.Fprintf(_w, "%d ", v+1)
+		}
+		fmt.Fprintln(_w)
+		fmt.Fprintf(_w, "%d ", ccnt)
+		for _, v := range C {
+			fmt.Fprintf(_w, "%d ", v+1)
+		}
+		fmt.Fprintln(_w)
+	}
+	M := MinInt(N, 8)
+	cnt := make(map[int]int)
+	for bit := 1; bit < 1<<M; bit++ {
+		sum := 0
+		for i := 0; i < N; i++ {
+			if bit>>i&1 == 1 {
+				sum += A[i]
+				sum %= 200
+			}
+		}
+		if v, ok := cnt[sum]; ok {
+			PrintBC(bit, v)
+			return
+		}
+		cnt[sum] = bit
+	}
+	fmt.Fprintf(_w, "No\n")
 }
 
 func Contains(x int, nums ...int) bool {
