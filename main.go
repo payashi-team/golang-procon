d162fc42013adcf0be81fff6fd892a81b5fe31f3
hiromichi_hayashi <42857417+payashi@users.noreply.github.com>
2022-04-19 22:58:20 +0000

    solve Ant Takahashi

--- main.go
+++ main.go
@@ -13,31 +13,56 @@ const (
 	MOD = 998244353
 )
 
-func main() {
-	defer _w.Flush()
-	var N int
-	fmt.Fscan(_r, &N)
-	ans := Solve(N)
-	fmt.Fprintf(_w, "%d\n", len(ans))
-	for _, v := range ans {
-		fmt.Fprintf(_w, "%d\n", v)
-	}
+type Point struct {
+	x, y float64
 }
 
-func Solve(N int) []int {
-	ret := make([]int, 0)
-	for i := MaxInt(N-9*18, 1); i <= N; i++ {
-		dsum := 0
-		num := i
-		for num > 0 {
-			dsum += num % 10
-			num /= 10
+func main() {
+	defer _w.Flush()
+	var p0 Point
+	fmt.Fscan(_r, &p0.x, &p0.y)
+	var N int
+	fmt.Fscan(_r, &N)
+	points := make([]Point, N)
+	for i := 0; i < N; i++ {
+		fmt.Fscan(_r, &points[i].x, &points[i].y)
+	}
+	ans := Solve(p0, N, points)
+	fmt.Fprintf(_w, "%.10f\n", ans)
+}
+
+func Solve(p0 Point, N int, points []Point) float64 {
+	ans := 1000.0
+	for i := 0; i < N; i++ {
+		p1 := points[i]
+		p2 := points[(i+1)%N]
+		s := -Dot(Minus(p1, p0), Minus(p2, p1)) / Dot(Minus(p2, p1), Minus(p2, p1))
+		if s < 0 || 1 < s {
+			continue
 		}
-		if dsum+i == N {
-			ret = append(ret, i)
+		p3 := Minus(Plus(Scale(p1, 1-s), Scale(p2, s)), p0)
+		dist := math.Sqrt(Dot(p3, p3))
+		if ans > dist {
+			ans = dist
 		}
 	}
-	return ret
+	return ans
+}
+
+func Dot(p1, p2 Point) float64 {
+	return p1.x*p2.x + p1.y*p2.y
+}
+
+func Scale(p Point, alpha float64) Point {
+	return Point{p.x * alpha, p.y * alpha}
+}
+
+func Plus(p1, p2 Point) Point {
+	return Point{p1.x + p2.x, p1.y + p2.y}
+}
+
+func Minus(p1, p2 Point) Point {
+	return Point{p1.x - p2.x, p1.y - p2.y}
 }
 
 func Contains(x int, nums ...int) bool {
