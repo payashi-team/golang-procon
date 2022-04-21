9a9b66ff4dc64972a82897f81c028fa68beeb1e0
hiromichi_hayashi <42857417+payashi@users.noreply.github.com>
2022-04-21 11:28:40 +0000

    solve Maximum and Minimum

--- main.go
+++ main.go
@@ -13,56 +13,29 @@ const (
 	MOD = 998244353
 )
 
-type Point struct {
-	x, y float64
-}
-
 func main() {
 	defer _w.Flush()
-	var p0 Point
-	fmt.Fscan(_r, &p0.x, &p0.y)
 	var N int
 	fmt.Fscan(_r, &N)
-	points := make([]Point, N)
+	D := make([]int, N)
 	for i := 0; i < N; i++ {
-		fmt.Fscan(_r, &points[i].x, &points[i].y)
+		fmt.Fscan(_r, &D[i])
 	}
-	ans := Solve(p0, N, points)
-	fmt.Fprintf(_w, "%.10f\n", ans)
+	max, min := Solve(N, D)
+	fmt.Fprintf(_w, "%d\n%d\n", max, min)
 }
 
-func Solve(p0 Point, N int, points []Point) float64 {
-	ans := 1000.0
-	for i := 0; i < N; i++ {
-		p1 := points[i]
-		p2 := points[(i+1)%N]
-		s := -Dot(Minus(p1, p0), Minus(p2, p1)) / Dot(Minus(p2, p1), Minus(p2, p1))
-		if s < 0 || 1 < s {
-			continue
-		}
-		p3 := Minus(Plus(Scale(p1, 1-s), Scale(p2, s)), p0)
-		dist := math.Sqrt(Dot(p3, p3))
-		if ans > dist {
-			ans = dist
-		}
+func Solve(N int, D []int) (int, int) {
+	sum := 0
+	e := MaxInt(D...)
+	for _, v := range D {
+		sum += v
 	}
-	return ans
-}
-
-func Dot(p1, p2 Point) float64 {
-	return p1.x*p2.x + p1.y*p2.y
-}
-
-func Scale(p Point, alpha float64) Point {
-	return Point{p.x * alpha, p.y * alpha}
-}
-
-func Plus(p1, p2 Point) Point {
-	return Point{p1.x + p2.x, p1.y + p2.y}
-}
-
-func Minus(p1, p2 Point) Point {
-	return Point{p1.x - p2.x, p1.y - p2.y}
+	if e <= sum-e {
+		return sum, 0
+	} else {
+		return sum, 2*e - sum
+	}
 }
 
 func Contains(x int, nums ...int) bool {
@@ -103,3 +76,4 @@ func MinInt(nums ...int) int {
 }
 
 var _r, _w = bufio.NewReader(os.Stdin), bufio.NewWriter(os.Stdout)
+
