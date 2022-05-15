4e1dc432be3c12d2e51f203d495f2371736871c1
hiromichi_hayashi <6phyphy6@gmail.com>
2022-05-15 14:04:28 +0000

    participate in ARC140

--- main.go
+++ main.go
@@ -15,52 +15,16 @@ const (
 
 func main() {
 	defer _w.Flush()
-	var N int
-	fmt.Fscan(_r, &N)
-	A := make([]int, N)
-	for i := 0; i < N; i++ {
-		fmt.Fscan(_r, &A[i])
+	var N, X int
+	fmt.Fscan(_r, &N, &X)
+	ans := Solve(N, X)
+	for _, v := range ans {
+		fmt.Fprintf(_w, "%d ", v)
 	}
-	ans := Solve(N, A)
-	fmt.Fprintf(_w, "%d\n", ans)
+	fmt.Fprintln(_w)
 }
 
-func Solve(N int, A []int) int {
-	calc := func(i, j int) (int, int) {
-		takahashi := 0
-		aoki := 0
-		if i > j {
-			i, j = j, i
-		}
-		for k := i; k <= j; k++ {
-			if (k-i)%2 == 0 {
-				takahashi += A[k]
-			} else {
-				aoki += A[k]
-			}
-		}
-		return takahashi, aoki
-	}
-	ret := -INF
-	// takahashi's pos
-	for i := 0; i < N; i++ {
-		aoki := -INF
-		maxj := -1
-		// aoki's pos
-		for j := 0; j < N; j++ {
-			if i == j {
-				continue
-			}
-			_, score := calc(i, j)
-			if score > aoki {
-				aoki = score
-				maxj = j
-			}
-		}
-		takahashi, _ := calc(i, maxj)
-		ret = MaxInt(ret, takahashi)
-	}
-	return ret
+func Solve(N, X int) []int {
 }
 
 func Contains(x int, nums ...int) bool {
