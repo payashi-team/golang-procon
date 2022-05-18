5b5760e951378b931c880e2db34b8ac8f6beb54e
hiromichi_hayashi <6phyphy6@gmail.com>
2022-05-18 01:51:22 +0000

    solve Unique Color

--- main.go
+++ main.go
@@ -16,24 +16,51 @@ const (
 
 func main() {
 	defer _w.Flush()
-	var N, M int
-	fmt.Fscanf(_r, "%d %d\n", &N, &M)
-	a, b, c := Solve(N, M)
-	fmt.Fprintf(_w, "%d %d %d\n", a, b, c)
+	var N int
+	fmt.Fscanf(_r, "%d\n", &N)
+	C := make([]int, N)
+	for i := 0; i < N; i++ {
+		fmt.Fscan(_r, &C[i])
+	}
+	edges := make([][]int, N)
+	for i := 0; i < N; i++ {
+		edges[i] = make([]int, 0)
+	}
+	for i := 0; i < N-1; i++ {
+		var a, b int
+		fmt.Fscan(_r, &a, &b)
+		a--
+		b--
+		edges[a] = append(edges[a], b)
+		edges[b] = append(edges[b], a)
+	}
+	ans := Solve(N, C, edges)
+	for _, v := range ans {
+		fmt.Fprintf(_w, "%d\n", v)
+	}
 }
 
-func Solve(N, M int) (int, int, int) {
-	if M < 2*N || 4*N < M {
-		return -1, -1, -1
-	}
-	for a := 0; a <= M/2; a++ {
-		b := -M + 4*N - 2*a
-		c := M - 3*N + a
-		if 0 <= b && 0 <= c {
-			return a, b, c
+func Solve(N int, C []int, edges [][]int) []int {
+	ret := make([]int, 0)
+	used := make([]bool, N)
+	var dfs func(int, map[int]int)
+	dfs = func(u int, mp map[int]int) {
+		if mp[C[u]] == 0 {
+			ret = append(ret, u+1)
 		}
+		mp[C[u]]++
+		used[u] = true
+		for _, v := range edges[u] {
+			if used[v] {
+				continue
+			}
+			dfs(v, mp)
+		}
+		mp[C[u]]--
 	}
-	return -1, -1, -1
+	dfs(0, make(map[int]int))
+	sort.Ints(ret)
+	return ret
 }
 
 // Both a and b are sorted
