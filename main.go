e845019b892e413a2167796f290a1ff43322b863
hiromichi_hayashi <6phyphy6@gmail.com>
2022-05-25 14:29:09 +0000

    solve 2-variable function

--- main.go
+++ main.go
@@ -15,62 +15,24 @@ const (
 
 func main() {
 	defer _w.Flush()
-	var N, M int
-	fmt.Fscan(_r, &N, &M)
-	uf := NewUnionFind(N)
-	for i := 0; i < M; i++ {
-		var a, b int
-		fmt.Fscan(_r, &a, &b)
-		a--
-		b--
-		uf.Unite(a, b)
-	}
-	mp := make(map[int]struct{})
-	for i := 0; i < N; i++ {
-		mp[uf.Root(i)] = struct{}{}
-	}
-	fmt.Fprintf(_w, "%d\n", len(mp)-1)
+	var N int
+	fmt.Fscan(_r, &N)
+	fmt.Fprintf(_w, "%d\n", Solve(N))
 }
 
-type UnionFind struct {
-	n             int
-	parent, depth []int
-}
+func Solve(N int) int {
 
-func NewUnionFind(n int) *UnionFind {
-	uf := new(UnionFind)
-	uf.n = n
-	uf.parent = make([]int, n)
-	uf.depth = make([]int, n)
-	for i := 0; i < n; i++ {
-		uf.parent[i] = i
+	f := func(a, b int) int {
+		return a*a*a + a*a*b + a*b*b + b*b*b
 	}
-	return uf
-}
-
-func (uf *UnionFind) Root(x int) int {
-	if x == uf.parent[x] {
-		return x
-	}
-	uf.parent[x] = uf.Root(uf.parent[x])
-	return uf.parent[x]
-}
-
-func (uf *UnionFind) Unite(x, y int) {
-	x = uf.Root(x)
-	y = uf.Root(y)
-
-	if x == y {
-		return
-	}
-	if uf.depth[x] < uf.depth[y] {
-		uf.parent[x] = y
-	} else {
-		uf.parent[y] = x
-		if uf.depth[x] == uf.depth[y] {
-			uf.depth[x]++
+	j := int(1e6)
+	X := INF
+	for i := 0; j >= 0 && i <= int(1e6); i++ {
+		for ; j >= 0 && f(i, j) >= N; j-- {
+			X = MinInt(X, f(i, j))
 		}
 	}
+	return X
 }
 
 func Contains(x int, nums ...int) bool {
