929a2d93e3d6272aae2deaaf4fdf1fc8e9d65337
hiromichi_hayashi <6phyphy6@gmail.com>
2022-06-04 23:03:49 +0000

    solve Rectangle GCD

--- main.go
+++ main.go
@@ -18,41 +18,78 @@ func main() {
 	defer _w.Flush()
 	_s.Split(bufio.ScanWords)
 	_s.Buffer([]byte{}, math.MaxInt32)
-	N, M := ScanInt(), ScanInt()
-	edges := make([][]int, N)
+	N, Q := ScanInt(), ScanInt()
+	A := make([]int, N)
+	B := make([]int, N)
 	for i := 0; i < N; i++ {
-		edges[i] = make([]int, 0)
+		A[i] = ScanInt()
 	}
-	for i := 0; i < M; i++ {
-		a, b := ScanInt()-1, ScanInt()-1
-		edges[a] = append(edges[a], b)
-		edges[b] = append(edges[b], a)
+	for i := 0; i < N; i++ {
+		B[i] = ScanInt()
 	}
-	Q := ScanInt()
-	for i := 0; i < Q; i++ {
-		x, k := ScanInt()-1, ScanInt()
-		que := make([]int, 0)
-		que = append(que, x)
-		used := []int{x}
-		for t := 0; t < k; t++ {
-			nextQue := make([]int, 0)
-			for _, u := range que {
-				for _, v := range edges[u] {
-					if Contains(v, used...) {
-						continue
-					}
-					nextQue = append(nextQue, v)
-					used = append(used, v)
-				}
-			}
-			que = nextQue
+	diffA := make([]int, N-1)
+	diffB := make([]int, N-1)
+	for i := 0; i < N-1; i++ {
+		diffA[i] = AbsInt(A[i+1] - A[i])
+		diffB[i] = AbsInt(B[i+1] - B[i])
+	}
+	sta := NewSegTree(N-1, diffA)
+	stb := NewSegTree(N-1, diffB)
+	for q := 0; q < Q; q++ {
+		h1, h2, w1, w2 := ScanInt()-1, ScanInt()-1, ScanInt()-1, ScanInt()-1
+		gcd := GCD(sta.Query(h1, h2), stb.Query(w1, w2))
+		gcd = GCD(gcd, A[h1]+B[w1])
+		fmt.Fprintf(_w, "%d\n", gcd)
+	}
+}
+
+type SegTree struct {
+	n     int
+	nodes []int
+}
+
+func NewSegTree(n int, nums []int) *SegTree {
+	st := new(SegTree)
+	st.n = 1
+	for st.n < n {
+		st.n *= 2
+	}
+	st.nodes = make([]int, st.n*2-1)
+	for i := 0; i < len(nums); i++ {
+		st.nodes[i+st.n-1] = nums[i]
+	}
+	for i := st.n - 2; i >= 0; i-- {
+		lv, rv := st.nodes[i*2+1], st.nodes[i*2+2]
+		st.nodes[i] = GCD(lv, rv)
+	}
+	return st
+}
+
+func (st *SegTree) Query(l, r int) int {
+	var dfs func(int, int, int) int
+	dfs = func(lb, ub, k int) int {
+		if l <= lb && ub <= r {
+			return st.nodes[k]
+		} else if r <= lb || ub <= l {
+			return 0
+		} else {
+			mid := (lb + ub) / 2
+			lv := dfs(lb, mid, 2*k+1)
+			rv := dfs(mid, ub, 2*k+2)
+			return GCD(lv, rv)
 		}
-		ret := 0
-		for _, v := range used {
-			ret += v + 1
-		}
-		fmt.Fprintf(_w, "%d\n", ret)
 	}
+	return dfs(0, st.n, 0)
+}
+
+func GCD(x, y int) int {
+	if x > y {
+		x, y = y, x
+	}
+	if x == 0 {
+		return y
+	}
+	return GCD(y%x, x)
 }
 
 func Contains(x int, nums ...int) bool {
