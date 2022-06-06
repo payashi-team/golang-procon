996e0f3c3f70291cc0503ec7cdce0fb60bd2968d
hiromichi_hayashi <6phyphy6@gmail.com>
2022-06-06 02:18:35 +0000

    solve Count 1's

--- main.go
+++ main.go
@@ -18,78 +18,38 @@ func main() {
 	defer _w.Flush()
 	_s.Split(bufio.ScanWords)
 	_s.Buffer([]byte{}, math.MaxInt32)
-	N, Q := ScanInt(), ScanInt()
-	A := make([]int, N)
-	B := make([]int, N)
+	N := ScanInt()
+	bits := make([]bool, N)
 	for i := 0; i < N; i++ {
-		A[i] = ScanInt()
-	}
-	for i := 0; i < N; i++ {
-		B[i] = ScanInt()
-	}
-	diffA := make([]int, N-1)
-	diffB := make([]int, N-1)
-	for i := 0; i < N-1; i++ {
-		diffA[i] = AbsInt(A[i+1] - A[i])
-		diffB[i] = AbsInt(B[i+1] - B[i])
-	}
-	sta := NewSegTree(N-1, diffA)
-	stb := NewSegTree(N-1, diffB)
-	for q := 0; q < Q; q++ {
-		h1, h2, w1, w2 := ScanInt()-1, ScanInt()-1, ScanInt()-1, ScanInt()-1
-		gcd := GCD(sta.Query(h1, h2), stb.Query(w1, w2))
-		gcd = GCD(gcd, A[h1]+B[w1])
-		fmt.Fprintf(_w, "%d\n", gcd)
-	}
-}
-
-type SegTree struct {
-	n     int
-	nodes []int
-}
-
-func NewSegTree(n int, nums []int) *SegTree {
-	st := new(SegTree)
-	st.n = 1
-	for st.n < n {
-		st.n *= 2
-	}
-	st.nodes = make([]int, st.n*2-1)
-	for i := 0; i < len(nums); i++ {
-		st.nodes[i+st.n-1] = nums[i]
-	}
-	for i := st.n - 2; i >= 0; i-- {
-		lv, rv := st.nodes[i*2+1], st.nodes[i*2+2]
-		st.nodes[i] = GCD(lv, rv)
-	}
-	return st
-}
-
-func (st *SegTree) Query(l, r int) int {
-	var dfs func(int, int, int) int
-	dfs = func(lb, ub, k int) int {
-		if l <= lb && ub <= r {
-			return st.nodes[k]
-		} else if r <= lb || ub <= l {
-			return 0
-		} else {
-			mid := (lb + ub) / 2
-			lv := dfs(lb, mid, 2*k+1)
-			rv := dfs(mid, ub, 2*k+2)
-			return GCD(lv, rv)
+		if ScanInt() == 1 {
+			bits[i] = true
 		}
 	}
-	return dfs(0, st.n, 0)
+	ans := Solve(N, bits)
+	fmt.Fprintf(_w, "%d\n", ans)
 }
 
-func GCD(x, y int) int {
-	if x > y {
-		x, y = y, x
+func Solve(N int, bits []bool) int {
+	nums := make([]int, N+1)
+	for i := 0; i < N; i++ {
+		if bits[i] {
+			nums[i+1] = nums[i] - 1
+		} else {
+			nums[i+1] = nums[i] + 1
+		}
 	}
-	if x == 0 {
-		return y
+	maxs := make([]int, N+1)
+	mins := make([]int, N+1)
+	for i := 0; i < N; i++ {
+		maxs[i+1] = MaxInt(maxs[i], nums[i])
+		mins[i+1] = MinInt(mins[i], nums[i])
 	}
-	return GCD(y%x, x)
+	var max, min int
+	for i := 0; i < N; i++ {
+		max = MaxInt(max, nums[i+1]-mins[i])
+		min = MinInt(min, nums[i+1]-maxs[i])
+	}
+	return max - min + 1
 }
 
 func Contains(x int, nums ...int) bool {
