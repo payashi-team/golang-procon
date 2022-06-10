059900cc194cd59f41494cdfe433712717415156
hiromichi_hayashi <6phyphy6@gmail.com>
2022-06-10 11:20:49 +0000

    solvee トリボナっち数列

--- main.go
+++ main.go
@@ -11,44 +11,67 @@ import (
 const (
 	INF = int(1 << 61)
 	// MOD = int(1e9 + 7)
-	MOD = 998244353
+	// MOD = 998244353
+	MOD = 10007
 )
 
 func main() {
 	defer _w.Flush()
-	var N, L int
-	fmt.Scan(&N, &L)
-	field := make([][]bool, L)
-	for i := 0; i < L; i++ {
-		field[i] = make([]bool, N-1)
-		_s.Scan()
-		S := _s.Text()
-		for j := 0; j < N-1; j++ {
-			field[i][j] = S[2*j+1] == '-'
-		}
-	}
-	_s.Scan()
-	T := _s.Text()
-	pos := -1
-	for j := 0; j < N; j++ {
-		if T[2*j] == 'o' {
-			pos = j
-			break
-		}
-	}
-	ans := Solve(N, L, field, pos)
+	N := ScanInt()
+	ans := Solve(N)
 	fmt.Fprintf(_w, "%d\n", ans)
 }
 
-func Solve(N, L int, field [][]bool, pos int) int {
-	for i := L - 1; i >= 0; i-- {
-		if pos-1 >= 0 && field[i][pos-1] {
-			pos--
-		} else if pos < N-1 && field[i][pos] {
-			pos++
+type Matrix [][]int
+
+func NewMatrix(n, m int) Matrix {
+	mtx := make(Matrix, n)
+	for i := 0; i < n; i++ {
+		mtx[i] = make([]int, m)
+	}
+	return mtx
+}
+
+// (n, m) * (m, l)
+func Multiple(a, b Matrix) Matrix {
+	n, m, l := len(a), len(a[0]), len(b[0])
+	c := NewMatrix(n, l)
+	for i := 0; i < n; i++ {
+		for j := 0; j < l; j++ {
+			for k := 0; k < m; k++ {
+				c[i][j] += a[i][k] * b[k][j]
+				c[i][j] %= MOD
+			}
 		}
 	}
-	return pos + 1
+	return c
+}
+
+func Power(a Matrix, p int) Matrix {
+	n := len(a)
+	ret := NewMatrix(n, n)
+	for i := 0; i < n; i++ {
+		ret[i][i] = 1
+	}
+	for p > 0 {
+		if p&1 == 1 {
+			ret = Multiple(ret, a)
+		}
+		a = Multiple(a, a)
+		p >>= 1
+	}
+	return ret
+}
+
+func Solve(N int) int {
+	A := [][]int{
+		{0, 1, 0},
+		{0, 0, 1},
+		{1, 1, 1},
+	}
+	A = Power(A, N-1)
+	return A[0][2]
+
 }
 
 func Contains(x int, nums ...int) bool {
