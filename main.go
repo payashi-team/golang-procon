60bba4438d44e09ebae0cfcc60712749382fa90e
hiromichi_hayashi <6phyphy6@gmail.com>
2022-07-01 02:49:57 +0000

    solve Hanjo

--- main.go
+++ main.go
@@ -17,78 +17,44 @@ const (
 var sc = bufio.NewScanner(os.Stdin)
 var wr = bufio.NewWriter(os.Stdout)
 
-type Point struct {
-	x, y int
-}
-
 func main() {
 	defer wr.Flush()
 	sc.Split(bufio.ScanWords)
 	sc.Buffer([]byte{}, math.MaxInt32)
-	st := Point{ni() - 1, ni() - 1}
-	W := nl()
-	field := make([][]int, 9)
-	for i := 0; i < 9; i++ {
-		field[i] = make([]int, 9)
-		nums := nl()
-		for j, c := range nums {
-			field[i][j] = int(c - '0')
-		}
-	}
-	ans := Solve(st, W, field)
-	fmt.Fprintf(wr, "%s\n", ans)
+	H, W, A, B := ni(), ni(), ni(), ni()
+	ans := Solve(H, W, A, B)
+	fmt.Fprintf(wr, "%d\n", ans)
+
 }
 
-func Solve(st Point, W string, field [][]int) string {
-	xfield := make([][]int, 3*9-2)
-	for i := 0; i < 3*9-2; i++ {
-		xfield[i] = make([]int, 3*9-2)
-		var i_ int
-		if i < 8 {
-			i_ = 8 - i
-		} else if i < 2*9-1 {
-			i_ = i - 8
-		} else {
-			i_ = 24 - i
+func Solve(H, W, A, B int) int {
+	ret := 0
+	var dfs func(int, int, int, int)
+	dfs = func(bit, pos, a, b int) {
+		if pos == H*W {
+			ret++
+			return
 		}
-		for j := 0; j < 3*9-2; j++ {
-			var j_ int
-			if j < 8 {
-				j_ = 8 - j
-			} else if j < 2*9-1 {
-				j_ = j - 8
-			} else {
-				j_ = 24 - j
-			}
-			xfield[i][j] = field[i_][j_]
+		if (bit>>pos)&1 == 1 {
+			dfs(bit, pos+1, a, b)
+			return
+		}
+		bit |= 1 << pos
+		// put a square tatami
+		if b-1 >= 0 {
+			dfs(bit, pos+1, a, b-1)
+		}
+		// put a long tatami horizontally
+		if a-1 >= 0 && pos%W != W-1 {
+			dfs(bit|1<<(pos+1), pos+2, a-1, b)
+		}
+		// put a long tatami vertically
+		if a-1 >= 0 && pos+W < H*W {
+			dfs(bit|1<<(pos+W), pos+1, a-1, b)
 		}
 	}
-	st.x += 8
-	st.y += 8
-	var d Point
-	switch W {
-	case "R":
-		d = Point{0, 1}
-	case "L":
-		d = Point{0, -1}
-	case "U":
-		d = Point{-1, 0}
-	case "D":
-		d = Point{1, 0}
-	case "RU":
-		d = Point{-1, 1}
-	case "RD":
-		d = Point{1, 1}
-	case "LU":
-		d = Point{-1, -1}
-	case "LD":
-		d = Point{1, -1}
-	}
-	ret := make([]byte, 4)
-	for i := 0; i < 4; i++ {
-		ret[i] = byte(xfield[st.y+i*d.x][st.x+i*d.y] + '0')
-	}
-	return string(ret)
+	dfs(0, 0, A, B)
+	return ret
 }
 
 func Contains(x int, nums ...int) bool {
