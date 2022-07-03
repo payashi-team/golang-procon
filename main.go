9ff02dd6055803b19a452cd5c9eefe17e52243b0
hiromichi_hayashi <6phyphy6@gmail.com>
2022-07-03 00:15:19 +0000

    solve Special Subsets

--- main.go
+++ main.go
@@ -5,14 +5,13 @@ import (
 	"fmt"
 	"math"
 	"os"
-	"sort"
 	"strconv"
 )
 
 const (
 	INF = int(1 << 61)
-	MOD = int(1e9 + 7)
-	// MOD = 998244353
+	// MOD = int(1e9 + 7)
+	MOD = 998244353
 )
 
 var sc = bufio.NewScanner(os.Stdin)
@@ -22,37 +21,77 @@ func main() {
 	defer wr.Flush()
 	sc.Split(bufio.ScanWords)
 	sc.Buffer([]byte{}, math.MaxInt32)
-	N, M, X, Y := ni(), ni(), ni(), ni()
-	A := make([]int, N)
-	B := make([]int, M)
+	N := ni()
+	F := make([]int, N)
 	for i := 0; i < N; i++ {
-		A[i] = ni()
+		F[i] = ni() - 1
 	}
-	for i := 0; i < M; i++ {
-		B[i] = ni()
-	}
-	ans := Solve(N, M, X, Y, A, B)
+	ans := Solve(N, F)
 	fmt.Fprintf(wr, "%d\n", ans)
-
 }
 
-func Solve(N, M, X, Y int, A, B []int) int {
-	pos := 0
-	ret := 0
-	for {
-		aidx := sort.Search(N, func(i int) bool { return pos <= A[i] })
-		if aidx == N {
-			break
-		}
-		pos = A[aidx] + X
-		bidx := sort.Search(M, func(i int) bool { return pos <= B[i] })
-		if bidx == M {
-			break
-		}
-		pos = B[bidx] + Y
-		ret++
+func Solve(N int, F []int) int {
+	E := make([]int, 2*N)
+	Einv := make([][]int, 2*N)
+	for i := 0; i < 2*N; i++ {
+		E[i] = -1
+		Einv[i] = make([]int, 0)
 	}
+	for i := 0; i < N; i++ {
+		E[i] = N + F[i]
+		E[i+N] = i
+	}
+	for i := 0; i < 2*N; i++ {
+		v := E[i]
+		if v < 0 {
+			continue
+		}
+		Einv[v] = append(Einv[v], i)
+	}
+	// SCC
+	cnt := 0
+	used := make([]bool, 2*N)
+	nums := make([]int, 0)
+	var dfs func(int)
+	dfs = func(u int) {
+		used[u] = true
+		v := E[u]
+		if v >= 0 && !used[v] {
+			dfs(v)
+		}
+		nums = append(nums, u)
+	}
+	for i := 0; i < 2*N; i++ {
+		if !used[i] {
+			dfs(i)
+		}
+	}
+	used = make([]bool, 2*N)
+	var dfs2 func(u int)
+	dfs2 = func(u int) {
+		used[u] = true
+		for _, v := range Einv[u] {
+			if used[v] {
+				continue
+			}
+			dfs2(v)
+		}
+	}
+	for i := 0; i < 2*N; i++ {
+		u := nums[i]
+		if !used[u] {
+			dfs2(u)
+			cnt++
+		}
+	}
+	ret := 1
+	for i := 0; i < cnt; i++ {
+		ret *= 2
+		ret %= MOD
+	}
+	ret--
 	return ret
+
 }
 
 func Contains(x int, nums ...int) bool {
