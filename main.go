3cb4ec73a489221ed1f29425766c91e6bacb6a83
hiromichi_hayashi <6phyphy6@gmail.com>
2022-07-04 03:48:45 +0000

    solve Lamps

--- main.go
+++ main.go
@@ -21,77 +21,44 @@ func main() {
 	defer wr.Flush()
 	sc.Split(bufio.ScanWords)
 	sc.Buffer([]byte{}, math.MaxInt32)
-	N := ni()
-	F := make([]int, N)
+	N, K := ni(), ni()
+	A := make([]int, N)
 	for i := 0; i < N; i++ {
-		F[i] = ni() - 1
+		A[i] = ni()
 	}
-	ans := Solve(N, F)
-	fmt.Fprintf(wr, "%d\n", ans)
+	ans := Solve(N, K, A)
+	for _, v := range ans {
+		fmt.Fprintf(wr, "%d ", v)
+	}
+	fmt.Fprintln(wr)
 }
 
-func Solve(N int, F []int) int {
-	E := make([]int, 2*N)
-	Einv := make([][]int, 2*N)
-	for i := 0; i < 2*N; i++ {
-		E[i] = -1
-		Einv[i] = make([]int, 0)
-	}
-	for i := 0; i < N; i++ {
-		E[i] = N + F[i]
-		E[i+N] = i
-	}
-	for i := 0; i < 2*N; i++ {
-		v := E[i]
-		if v < 0 {
-			continue
-		}
-		Einv[v] = append(Einv[v], i)
-	}
-	// SCC
-	cnt := 0
-	used := make([]bool, 2*N)
-	nums := make([]int, 0)
-	var dfs func(int)
-	dfs = func(u int) {
-		used[u] = true
-		v := E[u]
-		if v >= 0 && !used[v] {
-			dfs(v)
-		}
-		nums = append(nums, u)
-	}
-	for i := 0; i < 2*N; i++ {
-		if !used[i] {
-			dfs(i)
-		}
-	}
-	used = make([]bool, 2*N)
-	var dfs2 func(u int)
-	dfs2 = func(u int) {
-		used[u] = true
-		for _, v := range Einv[u] {
-			if used[v] {
-				continue
+func Solve(N, K int, A []int) []int {
+	for k := 0; k < K; k++ {
+		B := make([]int, N)
+		for i := 0; i < N; i++ {
+			l := i - A[i]
+			r := i + A[i] + 1
+			B[MaxInt(0, l)]++
+			if r < N {
+				B[r]--
 			}
-			dfs2(v)
+		}
+		for i := 0; i < N-1; i++ {
+			B[i+1] += B[i]
+		}
+		update := false
+		for i := 0; i < N; i++ {
+			A[i] = B[i]
+			if A[i] < N {
+				update = true
+			}
+		}
+		if !update {
+			break
 		}
 	}
-	for i := 0; i < 2*N; i++ {
-		u := nums[i]
-		if !used[u] {
-			dfs2(u)
-			cnt++
-		}
-	}
-	ret := 1
-	for i := 0; i < cnt; i++ {
-		ret *= 2
-		ret %= MOD
-	}
-	ret--
-	return ret
-
+	return A
 }
 
 func Contains(x int, nums ...int) bool {
