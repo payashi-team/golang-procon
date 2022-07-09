6f48582ee48f9b50aebffbc946c9f7d8fc9968f0
hiromichi_hayashi <6phyphy6@gmail.com>
2022-07-09 12:45:17 +0000

    solve 縞模様

--- main.go
+++ main.go
@@ -21,43 +21,35 @@ func main() {
 	defer wr.Flush()
 	sc.Split(bufio.ScanWords)
 	sc.Buffer([]byte{}, math.MaxInt32)
-	N, D, K := ni(), ni(), ni()
-	L := make([]int, D)
-	R := make([]int, D)
-	S := make([]int, K)
-	T := make([]int, K)
-	for i := 0; i < D; i++ {
-		L[i], R[i] = ni()-1, ni()-1
+	N, C := ni(), ni()
+	A := make([]int, N)
+	for i := 0; i < N; i++ {
+		A[i] = ni() - 1
 	}
-	for i := 0; i < K; i++ {
-		S[i], T[i] = ni()-1, ni()-1
-	}
-	ans := Solve(N, D, K, L, R, S, T)
-	for _, v := range ans {
-		fmt.Fprintf(wr, "%d\n", v)
-	}
+	ans := Solve(N, C, A)
+	fmt.Fprintf(wr, "%d\n", ans)
 }
 
-func Solve(N, D, K int, L, R, S, T []int) []int {
-	ret := make([]int, K)
-	for i := 0; i < D; i++ {
-		for j := 0; j < K; j++ {
-			if ret[j] > 0 {
+func Solve(N, C int, A []int) int {
+	ret := INF
+	for i := 0; i < 10; i++ {
+		for j := 0; j < 10; j++ {
+			if i == j {
 				continue
 			}
-			if L[i] <= S[j] && S[j] <= R[i] {
-				if L[i] <= T[j] && T[j] <= R[i] {
-					ret[j] = i + 1
-					S[j] = T[j]
-				} else if S[j] < T[j] {
-					S[j] = R[i]
-				} else {
-					S[j] = L[i]
+			cnt := 0
+			for k := 0; k < N; k++ {
+				if k&1 == 0 && A[k] != i {
+					cnt++
+				}
+				if k&1 == 1 && A[k] != j {
+					cnt++
 				}
 			}
+			ret = MinInt(ret, cnt)
 		}
 	}
-	return ret
+	return ret * C
 }
 
 func Contains(x int, nums ...int) bool {
