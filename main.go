a6d3019633049625ae3339052e97cdb688645f53
hiromichi_hayashi <6phyphy6@gmail.com>
2022-07-11 15:18:50 +0000

    solve Collision

--- main.go
+++ main.go
@@ -17,8 +17,8 @@ const (
 var sc = bufio.NewScanner(os.Stdin)
 var wr = bufio.NewWriter(os.Stdout)
 
-type Point struct {
-	x, y float64
+type Query struct {
+	c, d int
 }
 
 func main() {
@@ -26,27 +26,88 @@ func main() {
 	sc.Split(bufio.ScanWords)
 	sc.Buffer([]byte{}, math.MaxInt32)
 	N := ni()
-	P := make([]Point, N)
+	Q := ni()
+	edges := make([][]int, N)
 	for i := 0; i < N; i++ {
-		P[i].x, P[i].y = float64(ni()), float64(ni())
+		edges[i] = make([]int, 0)
 	}
-	ans := Solve(N, P)
-	fmt.Fprintf(wr, "%.9f\n", ans)
+	for i := 0; i < N-1; i++ {
+		a, b := ni()-1, ni()-1
+		edges[a] = append(edges[a], b)
+		edges[b] = append(edges[b], a)
+	}
+	qs := make([]Query, Q)
+	for i := 0; i < Q; i++ {
+		qs[i] = Query{ni() - 1, ni() - 1}
+	}
+	Solve(N, Q, edges, qs)
 }
 
-func Solve(N int, P []Point) float64 {
-	ret := -1.0
+func Solve(N, Q int, edges [][]int, qs []Query) {
+	parent := make([][]int, N) // parent[i][j+1] := parent[parent[i][j]][j]
+	K := 0
+	t := 1
+	for t < N {
+		t *= 2
+		K++
+	}
+	depth := make([]int, N)
 	for i := 0; i < N; i++ {
-		for j := 0; j < N; j++ {
-			dx := P[i].x - P[j].x
-			dy := P[i].y - P[j].y
-			dist := math.Sqrt(dx*dx + dy*dy)
-			if dist > ret {
-				ret = dist
+		parent[i] = make([]int, K)
+		parent[i][0] = -1
+	}
+	used := make([]bool, N)
+	var dfs func(int, int)
+	dfs = func(u, d int) {
+		used[u] = true
+		for _, v := range edges[u] {
+			if used[v] {
+				continue
+			}
+			parent[v][0] = u
+			depth[v] = d + 1
+			dfs(v, d+1)
+		}
+	}
+	dfs(0, 0)
+	for j := 0; j < K-1; j++ {
+		for i := 0; i < N; i++ {
+			if parent[i][j] == -1 {
+				parent[i][j+1] = -1
+			} else {
+				parent[i][j+1] = parent[parent[i][j]][j]
 			}
 		}
 	}
-	return ret
+	for _, q := range qs {
+		a, b := q.c, q.d
+		if depth[a] > depth[b] {
+			a, b = b, a
+		}
+		// b is deeper
+		diff := depth[b] - depth[a] // >=0
+		for i := 0; i < K; i++ {
+			if (diff>>i)&1 == 1 {
+				b = parent[b][i]
+			}
+		}
+		for i := K - 1; i >= 0; i-- {
+			if parent[a][i] != parent[b][i] {
+				a = parent[a][i]
+				b = parent[b][i]
+			}
+		}
+		c := parent[a][0]
+		dist := depth[q.c] + depth[q.d]
+		if c != -1 {
+			dist -= depth[c] * 2
+		}
+		if dist&1 == 1 {
+			fmt.Println("Road")
+		} else {
+			fmt.Println("Town")
+		}
+	}
 }
 
 func Contains(x int, nums ...int) bool {
