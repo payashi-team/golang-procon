3b6cf34d335473ddfa9c12118bca4f509097f375
hiromichi_hayashi <6phyphy6@gmail.com>
2022-07-23 13:51:44 +0000

    participate in ABC261

--- main.go
+++ main.go
@@ -21,38 +21,84 @@ func main() {
 	defer wr.Flush()
 	sc.Split(bufio.ScanWords)
 	sc.Buffer([]byte{}, math.MaxInt32)
-	a := float64(ni())
-	b := float64(ni())
-	d := float64(ni())
-	d = d / 360 * 2 * math.Pi
-	s, c := math.Sincos(d)
-	fmt.Fprintf(wr, "%.8f %.8f\n", a*c-b*s, a*s+b*c)
+	N := ni()
+	C := make([]int, N)
+	X := make([]int, N)
+	for i := 0; i < N; i++ {
+		C[i] = ni()
+	}
+	for i := 0; i < N; i++ {
+		X[i] = ni()
+	}
+	ans := Solve(N, C, X)
+	fmt.Fprintf(wr, "%d\n", ans)
 }
 
-func Solve(N int, R []int) int {
-	dp := make([][]int, 2)
-	for i := 0; i < 2; i++ {
-		dp[i] = make([]int, N)
-	}
-	// dp0 increasing
-	// dp1 decreasing
+func Solve(N int, C, X []int) int {
+	mp := make(map[int][]int)
 	for i := 0; i < N; i++ {
-		dp[0][i] = 1
-		dp[1][i] = 1
-		for j := 0; j < i; j++ {
-			if R[i] > R[j] {
-				dp[0][i] = MaxInt(dp[0][i], dp[1][j]+1)
-			} else if R[i] < R[j] {
-				dp[1][i] = MaxInt(dp[1][i], dp[0][j]+1)
-			}
+		mp[C[i]] = append(mp[C[i]], X[i])
+	}
+	ret := CountInv(N, X)
+	// for _, v := range mp {
+	// 	ret -= CountInv(N, v)
+	// }
+	return ret
+}
+
+func CountInv(N int, A []int) int {
+	st := NewSegTree(N + 1)
+	ret := 0
+	for i := 0; i < len(A); i++ {
+		st.Add(1, A[i])
+		fmt.Fprintf(wr, "(0, %d], %d\n", A[i], st.Query(0, A[i]+1))
+		ret += i + 1 - st.Query(0, A[i]+1)
+	}
+	return ret
+}
+
+type SegTree struct {
+	nodes []int
+	n     int
+}
+
+func NewSegTree(n int) *SegTree {
+	st := new(SegTree)
+	st.n = 1
+	for st.n < n {
+		st.n *= 2
+	}
+	st.nodes = make([]int, st.n*2-1)
+	return st
+}
+
+func (st *SegTree) Add(x, idx int) {
+	pos := idx + st.n - 1
+	for pos > 0 {
+		st.nodes[pos] += x
+		par := (pos - 1) / 2
+		sib := 4*par + 3 - pos
+		x = st.nodes[pos] + st.nodes[sib]
+		pos = par
+	}
+	st.nodes[0] += x
+}
+
+func (st *SegTree) Query(l, r int) int {
+	var dfs func(int, int, int) int
+	dfs = func(lb, ub, k int) int {
+		if l <= lb && ub <= r {
+			return st.nodes[k]
+		} else if r <= lb || ub <= l {
+			return 0
+		} else {
+			mid := (lb + ub) / 2
+			lv := dfs(lb, mid, 2*k+1)
+			rv := dfs(mid, ub, 2*k+2)
+			return lv + rv
 		}
 	}
-	ret := MaxInt(dp[0][N-1], dp[1][N-1])
-	if ret < 3 {
-		return 0
-	} else {
-		return ret
-	}
+	return dfs(0, st.n, 0)
 }
 
 type UFind struct {
